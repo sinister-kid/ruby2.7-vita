913bbbf60615590378fd0936d1b5462851f529cc
sinister_kid <auro_sk@ik.me>
2026-02-27 04:59:56 +0100

    Get into "static inline in header" way to Ruby's coroutines

--- coroutine/scefiber/Context.c
+++ coroutine/scefiber/Context.c
@@ -5,106 +5,22 @@
 #include <psp2/kernel/sysmem.h>
 #include <psp2/sysmodule.h>
 #include <stdint.h>
-#include <stdlib.h>
 
-#include "dlog.h"
+uint32_t fiber_counter = 1; // Root is 0, doesnt use fiber but does have context
 
-// How knows bitmask operations? Not me.
-#define TOGGLE_MASK(x, m) ((x) ^= (m))
-#define ADD_MASK(x, m) ((x) |= (m))
-#define SUB_MASK(x, m) ((x) &= ~(m))
-#define ANY_MASK(x, m) ((x) & (m))
-#define NONE_MASK(x, m) (((x) & (m)) == 0)
-#define ALL_MASK(x, m) (((x) & (m)) == (m))
-#define ONLY_MASK(x, m) (((x) & (m)) == (x))
-#define ANY_BUT_MASK(x, m, n) (((x) & ((m) | (n))) == (m))
-
-#define sceFiberInit _sceFiberInitializeImpl
-
-static ctx_t *current_ctx = NULL;
-static uint32_t fiber_counter = 1; // Root is 0, doesnt use fiber but does have context
-
-static inline ctx_t *to_ruby_ctx(uint32_t arg) { return (ctx_t *)(uintptr_t)arg; }
-static inline uint32_t to_vita_arg(void *p) { return (uint32_t)(uintptr_t)p; }
-
-
-static COROUTINE vita_fiber_entry(uint32_t argOnInitialize, uint32_t argOnRun) {
+COROUTINE vita_fiber_entry(uint32_t argOnInitialize, uint32_t argOnRun) {
     ctx_t *self = to_ruby_ctx(argOnInitialize);
     ctx_t *from = to_ruby_ctx(argOnRun);
 
-    current_ctx = self;
+    //current_ctx = self;
+    from->flags &= ~CURRENT;
+    self->flags |= CURRENT;
     self->entry(from, self);
 
     abort(); // Never look back
 }
 
-void coroutine_initialize(ctx_t *context, coroutine_start start,
-                          void *stack_base, size_t stack_size) {
-
-	//sceClibMemset(context, 0, sizeof(ctx_t));
-	
-	context->magic_cookie = 0xDEADBEEF;
-	context->switch_count = 0;
-	context->fiber_id = fiber_counter++;
-	context->flags = INITILIZED;
-    context->entry = start;
-	
-    DLOG("coroutine_initialize: ctx=%p id=%u", context, context->fiber_id);
-  	
-  	int status = sceFiberInit(&context->fiber, "ruby_scefiber", vita_fiber_entry,
-                            to_vita_arg(context), //self
-                            stack_base, stack_size, NULL);
-                            
-  	if (status != SCE_OK) {
-  	  	DLOG("coroutine_initialize: sceFiberInit failed with status %d", status);
-  	  	abort();
-  	}
-}
-
-static inline void fiber_finalize(ctx_t *c) {
-	DLOG("FINALIZE: v=%p v_id=%u", c, c->fiber_id);
-    sceFiberFinalize(&c->fiber);
-	c->flags = FINALIZED;
-}
-
-void coroutine_destroy(ctx_t *context) {
-	DLOG("coroutine_destroy: v=%p v_id=%u", context, context->fiber_id);
-  	if (ANY_BUT_MASK(context->flags, (ROOT | FINALIZED), INITILIZED)) {
-  	  	return;
-  	}
-  
-  	if (context == current_ctx) {
-  	  	context->flags |= PENDING;
-  	  	return;
-  	}
-    
-	fiber_finalize(context);
-}
-
-static inline void fiber_transfer(ctx_t *from, ctx_t *to) {
-  	if (from->flags & ROOT) {
-  	  	sceFiberRun(&to->fiber, to_vita_arg(from), NULL);
-  	  	return;
-  	}
-  
-  	if (to->flags & ROOT) {
-  	  	sceFiberReturnToThread(0, NULL);
-  	  	return;
-  	}
-
-  	sceFiberSwitch(&to->fiber, to_vita_arg(from), NULL);
-}
-
-void coroutine_transfer(ctx_t *from, ctx_t *to) {
-  	fiber_transfer(from, to);
-  	current_ctx = from;
-
-  	if (to->flags & PENDING) { 
-		fiber_finalize(to);
-	}
-}
-
 void Init_SceFiber() {
-  	DLOG("Init_SceFiber!");
-  	sceSysmoduleLoadModule(SCE_SYSMODULE_FIBER);
+    DLOG("Init_SceFiber!");
+    sceSysmoduleLoadModule(SCE_SYSMODULE_FIBER);
 }
--- coroutine/scefiber/Context.h
+++ coroutine/scefiber/Context.h
@@ -2,22 +2,35 @@
 #define COROUTINE_VITA_CONTEXT_H
 
 #include <sys/types.h>
-#include <setjmp.h>
 #include <psp2/fiber.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include "vita_mem_impl.h"
+#include "dlog.h"
+
+// How knows bitmask operations? Not me.
+#define TOGGLE_MASK(x, m) ((x) ^= (m))
+#define ADD_MASK(x, m) ((x) |= (m))
+#define SUB_MASK(x, m) ((x) &= ~(m))
+#define ANY_MASK(x, m) ((x) & (m))
+#define NONE_MASK(x, m) (((x) & (m)) == 0)
+#define ALL_MASK(x, m) (((x) & (m)) == (m))
+#define ONLY_MASK(x, m) (((x) & (m)) == (x))
+#define ANY_BUT_MASK(x, m, n) (((x) & ((m) | (n))) == (m))
 
 #define SCE_FIBER_CONTEXT_MINIMUM_SIZE  (512)
 #define SCE_FIBER_ALIGNMENT (8)
 #define SCE_FIBER_SIZE      (128)
 #define SCE_FIBER_MAX_NAME_LENGTH (31)
 #define SCE_OK (0)
+#define sceFiberInit _sceFiberInitializeImpl
 
 #if INTPTR_MAX <= INT32_MAX
 #define COROUTINE_LIMITED_ADDRESS_SPACE
 #endif
 
 #define COROUTINE __attribute__((noreturn)) void
+#define CORO_INLINE inline __attribute__((always_inline))
 
 struct coroutine_context; // forward decls
 
@@ -25,7 +38,8 @@ typedef enum FIBER_FLAGS {
     ROOT = (1 << 0),       // 1
     INITILIZED = (1 << 1), // 2
     PENDING = (1 << 2),    // 4
-    FINALIZED = (1 << 4)   // 8
+    FINALIZED = (1 << 4),  // 8
+    CURRENT = (1 << 8)     //16
 } fiber_flags_t;
 
 typedef COROUTINE(*coroutine_start)(struct coroutine_context *from, struct coroutine_context *self);
@@ -34,28 +48,77 @@ typedef struct coroutine_context {
     SceFiber fiber; // 128 bytes
     fiber_flags_t flags;
     uint32_t fiber_id;
-    uint32_t switch_count;
-    uint32_t magic_cookie;
     coroutine_start entry;
-} ctx_t SCE_ALIGN(8); //__attribute__((aligned(8)));
+} ctx_t SCE_ALIGN(8);
 
-//void coroutine_initialize_main(ctx_t *ctx);
+extern uint32_t fiber_counter;
 
-static inline void coroutine_initialize_main(ctx_t *context) {
-	context->flags = ROOT;
-	context->magic_cookie = 0xDEADBEEF;
-	context->switch_count = 0;
-	context->fiber_id = 0;
+void Init_SceFiber();
+COROUTINE vita_fiber_entry(uint32_t argOnInitialize, uint32_t argOnRun);
+
+static CORO_INLINE ctx_t *to_ruby_ctx(uint32_t arg) { return (ctx_t *)(uintptr_t)arg; }
+static CORO_INLINE uint32_t to_vita_arg(void *p) { return (uint32_t)(uintptr_t)p; }
+
+static CORO_INLINE void coroutine_initialize_main(ctx_t *context) {
+    context->flags = ( ROOT | CURRENT );
+    context->fiber_id = 0;
 }
 
-void coroutine_initialize(ctx_t *ctx,
-                          coroutine_start start,
-                          void *stack,
-                          size_t size);
+static CORO_INLINE void coroutine_initialize(ctx_t *context, coroutine_start start,
+                                        void *stack_base, size_t stack_size) {
+    context->fiber_id = fiber_counter++;
+    context->flags = INITILIZED;
+    context->entry = start;
+    
+    DLOG("coroutine_initialize: ctx=%p id=%u", context, context->fiber_id);
+    
+    int status = sceFiberInit(&context->fiber, "ruby_scefiber", vita_fiber_entry,
+                            to_vita_arg(context), //self
+                            stack_base, stack_size, NULL);
+    
+    if (status != SCE_OK) {
+        DLOG("coroutine_initialize: sceFiberInit failed with status %d", status);
+        abort();
+    }
+}
 
-void coroutine_transfer(ctx_t *from, ctx_t *to);
-void coroutine_destroy(ctx_t *ctx);
-void Init_SceFiber();
+static CORO_INLINE void fiber_transfer(ctx_t *from, ctx_t *to) {
+    if (from->flags & ROOT) return (void)sceFiberRun(&to->fiber, to_vita_arg(from), NULL);
+    if (to->flags & ROOT)   return (void)sceFiberReturnToThread(0, NULL);
+    sceFiberSwitch(&to->fiber, to_vita_arg(from), NULL);
+}
+
+static CORO_INLINE void fiber_finalize(ctx_t *c) {
+    DLOG("FINALIZE: v=%p v_id=%u", c, c->fiber_id);
+    sceFiberFinalize(&c->fiber);
+    c->flags = FINALIZED;
+}
+
+static CORO_INLINE void coroutine_transfer(ctx_t *from, ctx_t *to) {
+    fiber_transfer(from, to);
+
+    from->flags |= CURRENT;
+
+    if (to->flags & PENDING) { 
+        fiber_finalize(to);
+        return;
+    }
+
+    to->flags &= ~CURRENT;
+}
+
+
+static CORO_INLINE void coroutine_destroy(ctx_t *context) {
+    DLOG("coroutine_destroy: v=%p v_id=%u", context, context->fiber_id);
+    if (ANY_BUT_MASK(context->flags, (ROOT | FINALIZED), INITILIZED)) return;
+  
+    if (context->flags & CURRENT) {
+        context->flags |= PENDING;
+        return;
+    }
+    
+    fiber_finalize(context);
+}
 
 static inline size_t vita_adjust_fiber_machine_stack_size(size_t size) {
     if (size < SCE_FIBER_CONTEXT_MINIMUM_SIZE) {
